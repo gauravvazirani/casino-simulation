9ef807480f8fae4a2267099028341f0c07d1592a
gauravvazirani <gaurav9022@gmail.com>
2021-05-31 22:03:19 +0000

    Refactored Code to make simulation simpler

--- casino/src/player1326_state.py
+++ casino/src/player1326_state.py
@@ -1,15 +1,30 @@
 from . import bet 
 
+# class PlayerStateFactory():
+    
+#     def __init__(self, player, outcome):
+#         self.values = {
+#             'nowins': PlayerNoWins(player, outcome),
+#             'onewin': PlayerOneWin(player, outcome),
+#             'twowins': PlayerTwoWins(player, outcome),
+#             'threewins': PlayerThreeWins(player, outcome)
+#         }
+
+#     def get(self, name):
+#         return self.values.get(name)
+
+
 class Player1326State():
     """
     A base class implementing the common features of a set of states used in 
     1-3-2-6 betting strategy.    
     """
 
-    def __init__(self, player, bet_multiple, outcome):
+    def __init__(self, player, bet_multiple, outcome, next_state_win):
         self.player = player 
         self.bet_multiple = bet_multiple
         self.outcome = outcome
+        self.next_state_win = next_state_win
 
     def currentBet(self):
         """
@@ -22,7 +37,7 @@ class Player1326State():
         """
         leaves implementation to the specific state classes. 
         """
-        return NotImplemented
+        return self.next_state_win
 
     def nextLost(self):
         """
@@ -32,40 +47,16 @@ class Player1326State():
 
 class PlayerNoWins(Player1326State):
     def __init__(self, player, outcome):
-        super().__init__(player, 1, outcome)
-
-    def nextWon(self):
-        """
-        :return: (state) resets the current state of the player to one win.
-        """
-        return PlayerOneWin(self.player, self.outcome)
+        super().__init__(player, 1, outcome, PlayerOneWin(self.player, self.outcome))
 
 class PlayerOneWin(Player1326State):
     def __init__(self, player, outcome):
-        super().__init__(player, 3, outcome)
-
-    def nextWon(self):
-        """
-        :return: (state) resets the current state of the player to two wins.
-        """        
-        return PlayerTwoWins(self.player, self.outcome)
+        super().__init__(player, 3, outcome, PlayerTwoWins(self.player, self.outcome))
 
 class PlayerTwoWins(Player1326State):
     def __init__(self, player, outcome):
-        super().__init__(player, 2, outcome)
-
-    def nextWon(self):
-        """
-        :return: (state) resets the current state of the player to three wins.
-        """
-        return PlayerThreeWins(self.player, self.outcome)
+        super().__init__(player, 2, outcome, PlayerThreeWins(self.player, self.outcome))
 
 class PlayerThreeWins(Player1326State):
     def __init__(self, player, outcome):
-        super().__init__(player, 6, outcome)
-
-    def nextWon(self):
-        """
-        :return: (state) resets the current state of the player to no wins.
-        """
-        return PlayerNoWins(self.player, self.outcome)
+        super().__init__(player, 6, outcome, PlayerNoWins(self.player, self.outcome))
